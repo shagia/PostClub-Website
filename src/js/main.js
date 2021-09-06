2ae6147763b05e2e64abb2ee45d97d8457ab6f5f
shagia <usenet@shagia.me>
2021-09-06 09:55:02 -0700

    Milestone

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import {} from "./water.js";
-
+import pcLogo from "../img/POST-CLUB-LOGO-WHITE.png";
+const footerLogo = document.getElementById("footer-logo-src");
 console.log("Hello world");
 
 const testVariable = document.getElementById("test");
 
 testVariable.innerHTML = "Hello world works";
 testVariable.hidden = true;
+
+footerLogo.src = pcLogo;
