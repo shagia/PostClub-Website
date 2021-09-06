729deede5122b5d28cd41b0ae1c10aa39d4a4bd7
shagia <usenet@shagia.me>
2021-09-06 12:56:50 -0700

    Milestone 3

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,12 @@
 import {} from "./water.js";
 import pcLogo from "../img/POST-CLUB-LOGO-WHITE.png";
+import twitLogo from "../img/twitter_logo.png";
+import instaLogo from "../img/instagram_logo.png";
+import fceLogo from "../img/facebook_logo.png";
 const footerLogo = document.getElementById("footer-logo-src");
+const twitSrc = document.getElementsByClassName("twitter-logo-src")[0];
+const instaSrc = document.getElementsByClassName("instagram-logo-src")[0];
+const fceSrc = document.getElementsByClassName("facebook-logo-src")[0];
 console.log("Hello world");
 
 const testVariable = document.getElementById("test");
@@ -9,3 +15,6 @@ testVariable.innerHTML = "Hello world works";
 testVariable.hidden = true;
 
 footerLogo.src = pcLogo;
+twitSrc.src = twitLogo;
+instaSrc.src = instaLogo;
+fceSrc.src = fceLogo;
